4a03d3dc0d999ed9d4fc2b13defe4fd35be46faa
SAMELLA THAWANNE SILVA SANTOS e ISABELLY CAMILLY DA SILVA <thawannesamella20@gmail.com>
2026-05-13 15:43:49 -0300

    Add files via upload

---
+++ desafio5/script.js
@@ -0,0 +1,22 @@
+// DESAFIO 3 — PROMOÇÃO ESPECIAL
+
+// Crie variáveis para:
+// clienteVip
+// possuiCupom
+
+// O sistema deve verificar se:
+// O cliente é VIP
+// OU possui cupom de desconto
+
+// Se uma das condições for verdadeira:
+// Mostrar "Desconto aplicado"
+
+// Caso contrário:
+// Mostrar "Sem desconto disponível"
+let clientevip = true 
+let possuicupom = false 
+if (clientevip || possuicupom) {
+    console.log("desconto aplicado")
+} else {
+    console.log("Sem desconto disponível")
+}
